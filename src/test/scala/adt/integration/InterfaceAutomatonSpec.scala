ea7510c2de6f336cd7f8ccf95841b3f116879401
Tilman Zuckmantel <dbs476@ku.dk>
2022-11-18 11:28:45 +0100

    Adds functional single graph integration test

--- src/test/scala/adt/integration/InterfaceAutomatonSpec.scala
+++ src/test/scala/adt/integration/InterfaceAutomatonSpec.scala
@@ -2,12 +2,12 @@ package com.github.tizuck
 package adt.integration
 
 
+import dot.DotRepresentation
+import dot.DotRepresentation.RepresentationCtx
 import jsonGraphSchema.{Node, SimpleEdge, TopLevelSingleGraph}
 
-import com.github.tizuck.dot.DotRepresentation
-import com.github.tizuck.dot.DotRepresentation.RepresentationCtx
-import io.circe.parser.parse
 import io.circe._
+import io.circe.parser.parse
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.refspec.RefSpec
 import scalax.collection.edge.LDiEdge
@@ -15,6 +15,15 @@ import scalax.collection.io.dot.{DotAttr, DotEdgeStmt, DotGraph, DotRootGraph, I
 
 import scala.io.BufferedSource
 
+/**
+ * Tests the parsing and Dot Representation of a Single Graph
+ * containing 3 nodes and edges between these note with metadata of different type with a common superclass.
+ *
+ * This test shows exemplary how to define an AST for the metadata types and construct a custom decoder for metadata
+ * json content and how to use this decoder when parsing the json file.
+ * Further, an edgeTransformer is constructed that shows how to customly transform edges into
+ * a desired printed form containing HTML like strings.
+ */
 class InterfaceAutomatonSpec extends RefSpec with Matchers {
   object `testing the parsing and dot representation of an interface automaton` {
 
@@ -28,11 +37,21 @@ class InterfaceAutomatonSpec extends RefSpec with Matchers {
       }
 
       sealed case class GraphContext(spawnId:Int,transactionId:Int,keyValRef:List[Int])
+
       sealed case class GraphMetaData(ctx:GraphContext)
+
       sealed case class Context(spawnId:Int,transactionId:Int)
+
       sealed case class Variable(name:String,ctx:Context)
+
       sealed case class ReceiveEventId(variable:Variable, override val operationCtx: Context) extends Event
-      sealed case class ReadVar(variable: Variable, keyValRef:Int, resultVariable:Variable, override val operationCtx: Context) extends Event
+
+      sealed case class ReadVar(variable: Variable,
+                                keyValRef:Int,
+                                resultVariable:Variable,
+                                override val operationCtx: Context)
+        extends Event
+
       sealed case class EventSendVarId(override val operationCtx: Context,variableLookUp:Variable) extends Event
     }
 
@@ -155,7 +174,7 @@ class InterfaceAutomatonSpec extends RefSpec with Matchers {
         import helper._
 
         def varToCell(variable: ast.Variable): String = {
-          s"(${variable.name},${variable.ctx.spawnId},${variable.ctx.transactionId}"
+          s"(${variable.name},${variable.ctx.spawnId},${variable.ctx.transactionId})"
         }
 
 
@@ -169,14 +188,14 @@ class InterfaceAutomatonSpec extends RefSpec with Matchers {
                   case Some(event) => event match {
                       case ast.EventSendVarId(operationCtx, variableLookUp) =>
                         Table.empty
-                          .appendRow(Row(List("type",s"${EventSendVarId.getClass.toString}")))
+                          .appendRow(Row(List("type","EventSendVarId")))
                           .appendRow(Row(List("ia-name",s"${s.label.getOrElse("")}!")))
                           .appendRow(Row(List("lookup-var",varToCell(variableLookUp))))
                           .appendRow(Row(List("op-ctx",s"${operationCtx.transactionId},${operationCtx.spawnId}")))
                           .toHTML
                       case ast.ReadVar(variable, keyValRef, resultVariable, operationCtx) =>
                         Table.empty
-                          .appendRow(Row(List("type",s"${ReadVar.getClass.toString}")))
+                          .appendRow(Row(List("type","ReadVar")))
                           .appendRow(Row(List("ia-name",s"${s.label.getOrElse("")};")))
                           .appendRow(Row(List("variable", varToCell(variable))))
                           .appendRow(Row(List("keyValStore-ID", s"$keyValRef")))
@@ -210,16 +229,83 @@ class InterfaceAutomatonSpec extends RefSpec with Matchers {
 
           val dotRepresentation = DotRepresentation(topLevelGraphContainer,ctx)
 
-          val expectedValue =
+          val q1_q2_table =
             """
+              |<<table>
+              | <tr><td>type</td><td>ReadVar</td></tr>
+              | <tr><td>ia-name</td><td>read;</td></tr>
+              | <tr><td>variable</td><td>(c,0,0)</td></tr>
+              | <tr><td>keyValStore-ID</td><td>0</td></tr>
+              | <tr><td>result-var</td><td>(res,0,0)</td></tr>
+              | <tr><td>op-ctx</td><td>0,0</td></tr>
+              |</table>>
+              |""".stripMargin.filterNot(_.isWhitespace)
+
+          val q0_q1_table =
+            """
+              |<<table>
+              | <tr><td>type</td><td>ReceiveEventId</td></tr>
+              | <tr><td>ia-name</td><td>req?</td></tr>
+              | <tr><td>variable</td><td>(c,0,0)</td></tr>
+              | <tr><td>op-ctx</td><td>0,0</td></tr>
+              |</table>>
+              |""".stripMargin.filterNot(_.isWhitespace)
+
+          val q2_q0_table =
+            """
+              |<<table>
+              | <tr><td>type</td><td>EventSendVarId</td></tr>
+              | <tr><td>ia-name</td><td>snd!</td></tr>
+              | <tr><td>lookup-var</td><td>(res,0,0)</td></tr>
+              | <tr><td>op-ctx</td><td>0,0</td></tr>
+              |</table>>
+              |""".stripMargin.filterNot(_.isWhitespace)
+
+          val expectedValueSorted =
+            s"""
               |digraph {
-              |  q1 -> q2 [label = <<table><tr><td>type</td><td>class com.github.tizuck.Main$ReadVar$</td></tr><tr><td>ia-name</td><td>read;</td></tr><tr><td>variable</td><td>(c,0,0)</td></tr><tr><td>keyValStore-ID</td><td>0</td></tr><tr><td>result-var</td><td>res, sId:0, tId:0</td></tr><tr><td>op-ctx</td><td>0,0</td></tr></table>>]
-              |	q0 -> q1 [label = <<table><tr><td>type</td><td>ReceiveEventId</td></tr><tr><td>ia-name</td><td>req?</td></tr><tr><td>variable</td><td>(c,0,0)</td></tr><tr><td>op-ctx</td><td>0,0</td></tr></table>>]
-              |	q2 -> q0 [label = <<table><tr><td>type</td><td>class com.github.tizuck.Main$EventSendVarId$</td></tr><tr><td>ia-name</td><td>snd?</td></tr><tr><td>lookup-var</td><td>res sId:0 tId:0</td></tr><tr><td>op-ctx</td><td>0,0</td></tr></table>>]
+              | q0 -> q1 [label = $q0_q1_table]
+              | q1 -> q2 [label = $q1_q2_table]
+              |	q2 -> q0 [label = $q2_q0_table]
               |}
               |""".stripMargin.filterNot(_.isWhitespace)
 
-          expectedValue should equal (dotRepresentation.dot.filterNot(_.isWhitespace))
+          val dot_sorted_without_WS = {
+            val linesSeparated = dotRepresentation.dot.linesIterator.toList
+            val linesSeparatedWithoutWS = linesSeparated.map(_.filterNot(_.isWhitespace))
+            //We should have 5 lines consisting of the three graph edges
+            //and two additional edges to syntactically open and close the graph
+            linesSeparated should have(length(5))
+            linesSeparatedWithoutWS.foreach(f => {
+              f should (
+                startWith ("q0->q1") or
+                  startWith("q1->q2") or
+                  startWith("q2->q0") or
+                  startWith("digraph") or
+                  startWith("}")
+              )
+            })
+
+            def findFirstNumber(s1: String) = {
+              """\d+""".r.findFirstIn(s1).map(_.toInt).get
+            }
+
+            val sortedLines = linesSeparatedWithoutWS.sorted((s1:String, s2:String) => { (s1,s2) match {
+              case ("digraph{",_) => -1
+              case (_,"digraph{") => 1
+              case ("}",_) => 1
+              case (_,"}") => -1
+              case (s1,s2) =>
+                val firstNumbers1 = findFirstNumber(s1)
+                val firstNumbers2 = findFirstNumber(s2)
+                firstNumbers1.compare(firstNumbers2)
+            }})
+            sortedLines.mkString
+          }
+
+          val expectedValueSortedWithoutWS = expectedValueSorted.filterNot(_.isWhitespace)
+
+          expectedValueSortedWithoutWS should equal (dot_sorted_without_WS)
 
       }}
   }
